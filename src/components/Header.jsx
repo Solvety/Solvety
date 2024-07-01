74f37bc57daa092743dc38137e8f962e8d1dcc23
[mahtot] <[mahtotgher13@gmail.com]>
2024-07-01 21:11:54 +0300

    path changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -74,7 +74,7 @@ function Header() {
             barClicked&&  <IoMdClose id="close-bar" onClick={handleClick} size={'2rem'}/>
           } 
           <Link to='/researcher/home'className='items' id="first-item">about us</Link>
-          <Link to='/signedUp/rewards'className='items' id="second-item">  contact us</Link>
+          <Link to='/signedUp/qwest-game'className='items' id="second-item">  contact us</Link>
           <Link to='/'className='items' id="third-item">  services<TiArrowSortedDown id="arrow" /></Link>
          <Link to='/login'className='items' id="fourth-item">LogIn</Link>
          
@@ -85,7 +85,7 @@ function Header() {
             <Link to='/' className='items logo'><img src={Logo}></img></Link>
          
             <Link to='/researcher/home' className='items' id="first-item">about us</Link>
-            <Link to='/signedUp/rewards' className='items' id="second-item">contact us</Link>
+            <Link to='/signedUp/qwest-game' className='items' id="second-item">contact us</Link>
             <Link to='/' className='items' id="third-item">services<TiArrowSortedDown id="arrow" /></Link>
             <Link to='/login' className='items' id="fourth-item">LogIn</Link>
            
